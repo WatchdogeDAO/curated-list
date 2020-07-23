9defdba5524b3db4acee221257f47d85a49f716d
Mauro <mauro@osn.global>
2020-07-23 03:07:54 -0300

    Customize config files

--- buidler.config.js
+++ buidler.config.js
@@ -1,18 +1,18 @@
-const { usePlugin } = require('@nomiclabs/buidler/config')
-const hooks = require('./scripts/buidler-hooks')
+const { usePlugin } = require("@nomiclabs/buidler/config");
+const hooks = require("./scripts/buidler-hooks");
 
-usePlugin('@aragon/buidler-aragon')
+usePlugin("@aragon/buidler-aragon");
 
 module.exports = {
   // Default Buidler configurations. Read more about it at https://buidler.dev/config/
-  defaultNetwork: 'localhost',
+  defaultNetwork: "localhost",
   networks: {
     localhost: {
-      url: 'http://localhost:8545',
+      url: "http://localhost:8545",
     },
   },
   solc: {
-    version: '0.4.24',
+    version: "0.4.24",
     optimizer: {
       enabled: true,
       runs: 10000,
@@ -20,15 +20,15 @@ module.exports = {
   },
   // Etherscan plugin configuration. Learn more at https://github.com/nomiclabs/buidler/tree/master/packages/buidler-etherscan
   etherscan: {
-    apiKey: '', // API Key for smart contract verification. Get yours at https://etherscan.io/apis
+    apiKey: "9Q695S99Z1J2FEEQNK838XK9JPKR974WA3", // API Key for smart contract verification. Get yours at https://etherscan.io/apis
   },
   // Aragon plugin configuration
   aragon: {
     appServePort: 8001,
     clientServePort: 3000,
-    appSrcPath: 'app/',
-    appBuildOutputPath: 'dist/',
-    appName: 'counter-last',
+    appSrcPath: "app/",
+    appBuildOutputPath: "dist/",
+    appName: "list",
     hooks, // Path to script hooks
   },
-}
+};
